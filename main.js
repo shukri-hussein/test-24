81dedd0efbbe718194f541d73c642101c8140df8
shaku-hussien <nadahussein686@gmail.com>
2024-12-26 13:00:28 +0300

    day 24

---
+++ main.js
@@ -0,0 +1,58 @@
+// console.log("kafia");
+
+
+// let age = 100;
+// document.write(age);
+// console.log(age);
+
+
+
+// let age =20 ;
+// let name = ' shukri ';
+// var num1 =  2.5 ;
+// var bool =      true ;
+// console.log(age, name, num1, bool);
+// document.write(age , name, num1, bool)
+
+
+// arithmetic operations for js
+
+let add = 500 + 500;
+console.log(add);
+
+console.log('addition', (10 + 10));
+console.log('sub', (10 - 5));
+console.log('mul', (10 + 10));
+console.log('division', (100 / 5));
+console.log('expo', (10 ** 10));
+console.log('modulus', (10 % 10));
+
+let num1 = 99
+num1 ++;
+console.log('increment:', num1);
+
+let num2 = 100
+num2 --;
+console.log('decrement:', num2);
+
+// alert 
+alert(commit = "soo gali xogta")
+
+let num3 = 2
+let num4 = 3
+let num5 = num3 + num4
+alert(num5)
+
+
+let name = prompt('enter your name')
+let age = prompt('enter your age')
+console.log(name)
+console.log(age)
+
+
+let name1 = parseInt(prompt('number1'))
+let name2 = parseInt(prompt('number2'))
+console.log(name1 , name2)
+
+
+
---
+++ scrpt.js
@@ -0,0 +1,35 @@
+// let name = 'shukri sheikh hussein';
+// console.log(name);
+
+// let name1 = prompt('Enter Your Name');
+// console.log(name1);
+
+// let address = prompt('Enter Your address');
+// console.log(address);
+
+// let phone = prompt('Enter Your phone Number');
+// console.log(phone);
+
+// let brthday=parseInt( prompt('Enter Your birthday'));
+// let currentage=parseInt( prompt('Enter  Your current age'));
+// let age=currentage - brthday;
+// console.log(age);
+
+
+
+
+let name1 = prompt('Enter Your Name ');
+document.write(name1 );
+
+let address = prompt('Enter Your address');
+document.write(address);
+
+let phone = prompt('Enter Your phone Number');
+document.write(phone);
+
+let brthday=parseInt( prompt('Enter Your birthday'));
+let currentage=parseInt( prompt('Enter  Your current age'));
+let age=currentage - brthday;
+document.write(age);
+
+
